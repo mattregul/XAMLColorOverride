96486c37e27cb60b926941a75c8c3bb773304608
Matt Regul <dev.matt@pachead.com>
2016-03-03 15:04:27 -0500

    Added files via upload

---
+++ XAMLColorOverride/TestPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XAMLColorOverride
+{
+	public partial class TestPage : ContentPage
+	{
+
+		public TestPage()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
+
